f7cdc74bae2dc90e968e7de981f870b1480991e1
aabulhaj <aabulhaj1@gmail.com>
2019-03-24 13:50:57 +0200

    Implemented more fragment

--- app/src/main/java/com/aabulhaj/hujiapp/fragments/MoreFragment.kt
+++ app/src/main/java/com/aabulhaj/hujiapp/fragments/MoreFragment.kt
@@ -1,65 +1,50 @@
 package com.aabulhaj.hujiapp.fragments
 
-import android.content.Context
-import android.net.Uri
+import android.content.Intent
 import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-
+import android.support.v7.preference.Preference
+import android.support.v7.preference.PreferenceFragmentCompat
+import android.widget.Toast
 import com.aabulhaj.hujiapp.R
 
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [MoreFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [MoreFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class MoreFragment : Fragment() {
 
-    // TODO: Rename and change types of parameters
-    private var mParam1: String? = null
-    private var mParam2: String? = null
+class MoreFragment : PreferenceFragmentCompat() {
+    private val keyToMethodMap = HashMap<String, () -> Unit>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (arguments != null) {
-            mParam1 = arguments!!.getString(ARG_PARAM1)
-            mParam2 = arguments!!.getString(ARG_PARAM2)
+        addPreferencesFromResource(R.xml.more)
+
+        keyToMethodMap["contact_us"] = this::contactUs
+//        keyToMethodMap["licenses"] = start(LicensesActivity::class.java)
+//        keyToMethodMap["academic_calendar"] = start(CalendarActivity::class.java)
+//        keyToMethodMap["contacts"] = start(ContactsActivity::class.java)
+//        keyToMethodMap["campus_shuttle_times"] = start(ShuttleActivity::class.java)
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+
+    }
+
+    override fun onPreferenceTreeClick(preference: Preference): Boolean {
+        if (preference.key != null && keyToMethodMap.containsKey(preference.key)) {
+            keyToMethodMap[preference.key]?.invoke()
         }
+        return super.onPreferenceTreeClick(preference)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_more, container, false)
-    }
-
-    companion object {
-        // TODO: Rename parameter arguments, choose names that match
-        // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-        private val ARG_PARAM1 = "param1"
-        private val ARG_PARAM2 = "param2"
-
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MoreFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        fun newInstance(param1: String, param2: String): MoreFragment {
-            val fragment = MoreFragment()
-            val args = Bundle()
-            args.putString(ARG_PARAM1, param1)
-            args.putString(ARG_PARAM2, param2)
-            fragment.arguments = args
-            return fragment
+    private fun contactUs() {
+        val intent = Intent(Intent.ACTION_SEND_MULTIPLE)
+        intent.type = "message/rfc822"
+        intent.putExtra(android.content.Intent.EXTRA_EMAIL, arrayOf("aabulhaj1@gmail.com"))
+        try {
+            startActivity(Intent.createChooser(intent, getString(R.string.send_mail)))
+        } catch (ex: android.content.ActivityNotFoundException) {
+            Toast.makeText(activity, getString(R.string.no_email_accounts_error),
+                    Toast.LENGTH_SHORT).show()
         }
+
     }
-}// Required empty public constructor
+
+    private fun <T> start(target: Class<T>) = fun() = startActivity(Intent(context, target))
+}
